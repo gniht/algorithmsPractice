3771a863c0a33e2d96551c5a254d11785fc44af7
gniht <jasonthomasjat@gmail.com>
2022-08-09 09:42:09 -0700

    moved fib func out of indexjs and into module

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
-
+import { fib } from "./fibonacci.js";
 
 const body = document.querySelector("body");
 const output = document.createElement("div");
 body.appendChild(output);
 
-function fib(n) {  
-  if(n == 0 || n == 1){
-    return n;
-  }
-  return fib(n-1) + fib(n-2);  
-}
+let num1 = 10;
 
-
-let num = 10;
-
-
-output.innerText = `Fibonacci of ${num} is ${fib(num)}.`;
+output.innerText = `Fibonacci of ${num1} is ${fib(num1)}.`;
